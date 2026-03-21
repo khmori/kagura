fa5a6511eeb9eb494eaac9a6b16971f9d517ba75
khmori <tanabechankun@gmail.com>
2026-03-21 11:49:13 -0700

    chore: generate backend spring boot project structure

---
+++ backend/src/main/java/com/khmori/kagura/KaguraApplication.java
@@ -0,0 +1,13 @@
+package com.khmori.kagura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaguraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/khmori/kagura/KaguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khmori.kagura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
